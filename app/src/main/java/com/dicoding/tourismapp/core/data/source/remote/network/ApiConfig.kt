0d5a3b213d937272a86d2c920edfc6d113263def
rifaset123 <rifaindra123456@gmail.com>
2025-05-18 14:57:34 +0700

    feat: logging interceptor

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
@@ -1,14 +1,27 @@
 package com.dicoding.tourismapp.core.data.source.remote.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
+    // show response result on logcat (logging interceptor)
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
 
     fun provideApiService(): ApiService {
         val retrofit = Retrofit.Builder()
+            // API should be hidden, this is public API for learning purpose
             .baseUrl("https://tourism-api.dicoding.dev/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient()) // add logging interceptor (type "okhttp" to see logcat)
             .build()
         return retrofit.create(ApiService::class.java)
     }
