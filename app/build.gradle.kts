834668817850378083d94db5d1f4c6005073aebb
rifaset123 <rifaindra123456@gmail.com>
2025-05-31 22:07:13 +0700

    feat: update project with obfuscation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,11 +21,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true // obfuscation enabled
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
         }
     }
     compileOptions {
